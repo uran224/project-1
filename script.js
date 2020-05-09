a44b417779a14b52ee57758febe30fc9815c1bb9
uran224 <uran22@gmail.com>
2020-05-10 00:20:31 +0300

    The 1st commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// 'use strict';
+// let answer = prompt ("Do you see this?", "Yes");
+
+var money = prompt("Ваш бюджет на месяц, руб.", 40000);
+var time = prompt("Введите дату в формате YYYY-MM-DD", "2020-05-09");
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expensesObligatory: "",
+    expensesOptional: "",
+    extraIncome: [],
+    savings: false
+
+};
+
+appData.expensesObligatory = prompt("Введите обязательную статью расходов в этом месяце", "Dolgam.Net");
+var expensesObligatorySum = prompt("Во сколько обойдётся?", 15000);
+
+var expenses = {
+    answer_1: appData.expensesObligatory,
+    answer_2: expensesObligatorySum
+};
+
+var dailyBudget = Math.floor((appData.budget - expensesObligatorySum) / 30);
+
+alert ("Ваш дневной бюджет не более "+ dailyBudget + " рублей!");
+
+
+
+
