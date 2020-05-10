0903dd97a051401923f4ebe6584e9be2a189ecb2
uran224 <uran22@gmail.com>
2020-05-10 12:29:02 +0300

    appData.expenses[answer_1] = answer_2;

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 // 'use strict';
 // let answer = prompt ("Do you see this?", "Yes");
 
-var money = prompt("Ваш бюджет на месяц, руб.", 40000);
-var time = prompt("Введите дату в формате YYYY-MM-DD", "2020-05-09");
+let money = prompt("Ваш бюджет на месяц, руб.", 40000),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "2020-05-09");
 
-var appData = {
+let appData = {
     budget: money,
     timeData: time,
-    expensesObligatory: "",
-    expensesOptional: "",
-    extraIncome: [],
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
     savings: false
-
 };
 
-appData.expensesObligatory = prompt("Введите обязательную статью расходов в этом месяце", "Регулярный");
-var expensesObligatorySum = prompt("Во сколько обойдётся?", 15000);
+let answer_1 = prompt("Введите обязательную статью расходов в этом месяце", "Dolgam.net"),
+    answer_2 = prompt("Во сколько обойдётся?", 15000),
+    answer_3 = prompt("Введите обязательную статью расходов в этом месяце", "Квартплата"),
+    answer_4 = prompt("Во сколько обойдётся?", 5500);
 
-var expenses = {
-    answer_1: appData.expensesObligatory,
-    answer_2: expensesObligatorySum
-};
-
-var dailyBudget = Math.floor((appData.budget - expensesObligatorySum) / 30);
-
-alert ("Ваш дневной бюджет не более "+ dailyBudget + " рублей!");
+    appData.expenses[answer_1] = answer_2;
+appData.expenses[answer_3] = answer_4;
 
 
-
-
+alert ("Ваш бюджет на 1 день "+ Math.floor(appData.budget / 30) + " рублей!");
